1c7cea20b69cd14c220ac979cfaecab55ae47a7e
KP - Paret <kristjanomeele@gmail.com>
2025-06-08 23:40:05 +0300

    Learning to add blocks and items, mostly following guide and tutorial, no self work shiii. Seems to be working fine, blocks dont have drops but thats alr for now, items work and exist, no crafting yet

--- src/main/java/net/paret/landosmod/LandosMod.java
+++ src/main/java/net/paret/landosmod/LandosMod.java
@@ -1,6 +1,7 @@
 package net.paret.landosmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +14,8 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.paret.landosmod.block.ModBlocks;
+import net.paret.landosmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -30,6 +33,8 @@ public class LandosMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -43,7 +48,13 @@ public class LandosMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.STEELINGOT);
+            event.accept(ModItems.STEELUNFIRED);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.STEEL_BLOCK);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
